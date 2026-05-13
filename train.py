8ceb58696b27314a8920a46330597af200134114
jose <joseantoniogarcia@ctnaval.com>
2026-05-13 09:54:40 +0200

    tested augmenter

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+from pprint import pprint
 
 import torch
 import lejepa
@@ -6,6 +7,11 @@ import lejepa
 from src.utils import load_cfg, init_encoder
 from src.data.dataset import TextDataset
 from src.data.byte_tokenizer import BaselineTokenizer
+from src.aug.augmentations import Augmentations
+
+
+# TODO: think about where tokenization is handled (create padding utils than can be reusable and padding logic is not attached to tokenizers)
+# this way: inside the tokenizer we call padding utils, after that we perform augmentations and then we apply padding logic to each view
 
 if __name__ == "__main__":
     cfg = load_cfg("cfg.yml")
@@ -16,7 +22,9 @@ if __name__ == "__main__":
     else:
         raise ValueError(f"{cfg['tokenizer']['type']} is currently not implemented")
 
-    text_dataset = TextDataset(cfg=cfg, tokenizer=tokenizer)
+    augmenter = Augmentations(cfg=cfg)
 
-    print(text_dataset[0])
+    text_dataset = TextDataset(cfg=cfg, tokenizer=tokenizer, augmenter=augmenter)
+
+    pprint(text_dataset[0])
 

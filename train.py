9be9653b8aee3ea1d6340f71d565df1ea369b92c
jose <joseantoniogarcia@ctnaval.com>
2026-05-15 21:58:10 +0200

    tested forward with loader

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from pprint import pprint
 
 import torch
 import lejepa
+from torch.utils.data import DataLoader
 
 from src.utils import load_cfg, init_encoder
 from src.data.dataset import TextDataset
@@ -26,8 +27,22 @@ if __name__ == "__main__":
     augmenter = Augmentations(cfg=cfg)
 
     text_dataset = TextDataset(cfg=cfg, tokenizer=tokenizer, augmenter=augmenter)
-
-    pprint(text_dataset[0])
+    dataloader = DataLoader(dataset=text_dataset, batch_size=2, shuffle=True)
+    first_batch = next(iter(dataloader))
 
     encoder = ByteModernBertEncoder(cfg=cfg)
+
+    with torch.no_grad():
+        global_input_ids=first_batch["global_crops"]
+        global_attn_mask=first_batch["global_masks"]
+        local_input_ids=first_batch["local_crops"]
+        local_attn_mask=first_batch["local_masks"]
+
+        z = encoder(
+             global_input_ids=global_input_ids,
+             global_attn_mask=global_attn_mask,
+             local_input_ids=local_input_ids,
+             local_attn_mask=local_attn_mask,
+             )
+        
 

1ce2609892bfde466f62c06877ab0ef796dc286f
jose <joseantoniogarcia@ctnaval.com>
2026-05-15 21:56:49 +0200

    modify detokenize also to not expect batches and remove old commented tokenize method

--- src/data/byte_tokenizer.py
+++ src/data/byte_tokenizer.py
@@ -10,39 +10,6 @@ class BaselineTokenizer(Tokenizer):
         super().__init__()
         self.cfg = cfg
 
-    # def tokenize(self, text: list[str]) -> dict:
-
-    #     # handle single input instead of a list of inputs
-    #     if type(text) == str:
-    #         text = [text]
-
-    #     # encode to bytes
-    #     batch_bytes = [list(record.encode("utf-8")) for record in text]
-
-    #     # handle truncation and padding
-    #     batch_bytes_curated = []
-    #     attention_masks = []
-    #     for record in batch_bytes:
-    #         if len(record) >= self.cfg["model"]["max_position_embeddings"]:  # truncate
-    #             truncated_record = record[:self.cfg["model"]["max_position_embeddings"]]
-    #             batch_bytes_curated.append(truncated_record)
-    #             attention_masks.append([1] * self.cfg["model"]["max_position_embeddings"])
-    #         else:
-    #             padded_record = record + [self.cfg["model"]["pad_token_id"]] * (self.cfg["model"]["max_position_embeddings"] - len(record)) 
-    #             batch_bytes_curated.append(padded_record)
-    #             attention_masks.append([1] * len(record) + [0] * (self.cfg["model"]["max_position_embeddings"] - len(record)))
-
-    #     # convert to tensor
-    #     input_ids = torch.tensor(batch_bytes_curated, dtype=torch.long)  # [B, T]
-    #     attention_masks = torch.tensor(attention_masks, dtype=torch.long)  # [B, T]
-
-    #     tokenized_text = {
-    #         "input_ids": input_ids,
-    #         "attention_mask": attention_masks
-    #     }
-
-    #     return tokenized_text
-
 
     def tokenize(self, text: str) -> dict:
         byte_text = list(text.encode("utf-8"))
@@ -67,13 +34,12 @@ class BaselineTokenizer(Tokenizer):
 
 
     def detokenize(self, tokenized_text: dict) -> list[str]:
+        """ Detokenizes the tokenized text """
         
         input_ids = tokenized_text["input_ids"]
-        attention_masks = tokenized_text["attention_mask"]
+        attention_mask = tokenized_text["attention_mask"]
 
-        decoded_text = []
-        for ids_row, mask_row in zip(input_ids, attention_masks):
-            valid_ids = ids_row[mask_row.bool()].tolist()
-            decoded_text.append(bytes(valid_ids).decode("utf-8"))
+        valid_ids = input_ids[attention_mask.bool()].tolist()
 
-        return decoded_text
+        return bytes(valid_ids).decode("utf-8")
+

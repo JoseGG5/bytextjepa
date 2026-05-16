8e46d1b2f8268cb61a746457e63efdea880d3813
jose <joseantoniogarcia@ctnaval.com>
2026-05-16 10:06:56 +0200

    setup wandb

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
-
+import os
 
 import torch
 from torch.utils.data import DataLoader
+from dotenv import load_dotenv
+import wandb
 
 from src.utils import load_cfg, init_encoder
 from src.data.dataset import TextDataset
@@ -14,35 +16,35 @@ from src.model.model import ByteModernBertEncoder
 # TODO: Implement train pipeline
 # TODO: Decide optimizer
 # TODO: Run pipeline on just one example to verify everything is fine (loss should go down to 0)
+# TODO: Add cuda support
 
 if __name__ == "__main__":
-    cfg = load_cfg("cfg.yml")
-    encoder = init_encoder(cfg=cfg)
 
+    load_dotenv()
+
+    # load master cfg
+    cfg = load_cfg("cfg.yml")
+
+    # setup w&b
+    run = wandb.init(
+        entity=os.getenv("WANDB_ENTITY"),
+        project=os.getenv("WANDB_PROJECT"),
+        config=cfg,
+    )
+
+    # setup tokenizer
     if cfg["tokenizer"]["type"] == "baseline":
         tokenizer = BaselineTokenizer(cfg=cfg)
     else:
         raise ValueError(f"{cfg['tokenizer']['type']} is currently not implemented")
 
+    # setup data modules
     augmenter = Augmentations(cfg=cfg)
-
-    text_dataset = TextDataset(cfg=cfg, tokenizer=tokenizer, augmenter=augmenter)
+    text_dataset = TextDataset(cfg=cfg, tokenizer=tokenizer, augmenter=augmenter, dev=True)
     dataloader = DataLoader(dataset=text_dataset, batch_size=2, shuffle=True)
-    first_batch = next(iter(dataloader))
+    
+    # setup encoder
+    encoder = ByteModernBertEncoder(cfg=cfg).to(cfg["exp"]["device"])
 
-    encoder = ByteModernBertEncoder(cfg=cfg)
-
-    with torch.no_grad():
-        global_input_ids=first_batch["global_crops"]
-        global_attn_mask=first_batch["global_masks"]
-        local_input_ids=first_batch["local_crops"]
-        local_attn_mask=first_batch["local_masks"]
-
-        z = encoder(
-             global_input_ids=global_input_ids,
-             global_attn_mask=global_attn_mask,
-             local_input_ids=local_input_ids,
-             local_attn_mask=local_attn_mask,
-             )
-        
+    
 
